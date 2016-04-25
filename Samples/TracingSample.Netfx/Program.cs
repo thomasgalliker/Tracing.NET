26a42303100d37943c7577f06554b21e8b67544c
thomasgalliker <thomas@bluewin.ch>
2016-04-25 22:00:33 +0200

    Add Netfx sample project

---
+++ Samples/TracingSample.Netfx/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tracing;
+
+namespace TracingSample.Netfx
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ITracer tracer = Tracer.Create(typeof(Program));
+            tracer.Debug("Some debug message");
+            tracer.Info("Some info message");
+        }
+    }
+}
