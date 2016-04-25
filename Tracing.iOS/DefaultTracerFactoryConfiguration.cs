38fa00b927844b8fcf063e17b78909daa231c9fd
thomasgalliker <thomas@bluewin.ch>
2016-04-25 21:28:37 +0200

    Add ConsoleTracer for iOS

---
+++ Tracing.iOS/DefaultTracerFactoryConfiguration.cs
@@ -0,0 +1,10 @@
+namespace Tracing
+{
+    internal class DefaultTracerFactoryConfiguration : IDefaultTracerFactoryConfiguration
+    {
+        public ITracerFactory GetDefaultTracerFactory()
+        {
+            return new ConsoleTracerFactory();
+        }
+    }
+}
