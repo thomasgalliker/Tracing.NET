765e170f358caf2e33d483d56628fbabbd64246e
thomasgalliker <thomas@bluewin.ch>
2016-04-25 21:40:04 +0200

    Add ConsoleTracer to Windows-based projects

---
+++ Tracing.Shared.WinRT/DefaultTracerFactoryConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tracing.Shared.WinRT
+{
+    internal class DefaultTracerFactoryConfiguration : IDefaultTracerFactoryConfiguration
+    {
+        public ITracerFactory GetDefaultTracerFactory()
+        {
+            return new DebugTracerFactory();
+        }
+    }
+}
