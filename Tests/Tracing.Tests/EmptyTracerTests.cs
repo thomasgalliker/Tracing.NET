9c0717ca9667dce5e36f3b740a18065ea7a03014
Thomas Galliker <thomas@bluewin.ch>
2018-11-18 17:58:56 +0100

    Refactor test project to support dotnet sdk

---
+++ Tests/Tracing.Tests/EmptyTracerTests.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+using Xunit;
+
+namespace Tracing.Tests
+{
+    [Collection("Tracing")]
+    public class EmptyTracerTests
+    {
+        [Fact]
+        public void WriteThrowsIfMessageIsNull()
+        {
+            EmptyTracer tracer = new EmptyTracer();
+
+            Assert.Throws<ArgumentNullException>(() => tracer.Write(Category.Debug, null));
+
+            Assert.Throws<ArgumentNullException>(() => tracer.Write(Category.Information, null));
+
+            Assert.Throws<ArgumentNullException>(() => tracer.Write(Category.Warning, null));
+
+            Assert.Throws<ArgumentNullException>(() => tracer.Write(Category.Error, null));
+        }
+
+        [Fact]
+        public void WriteNotThrowsIfExceptionIsNull()
+        {
+            EmptyTracer tracer = new EmptyTracer();
+
+            // TODO GATH: Remove? // TODO GATH: Remove?? Assert.IsNotThrown<Exception>(() => tracer.Write(Category.Debug, null, "message"));
+            // TODO GATH: Remove? // TODO GATH: Remove?? Assert.IsNotThrown<Exception>(() => tracer.Write(Category.Information, null, "message"));
+            // TODO GATH: Remove? // TODO GATH: Remove?? Assert.IsNotThrown<Exception>(() => tracer.Write(Category.Warning, null, "message"));
+            // TODO GATH: Remove? // TODO GATH: Remove?? Assert.IsNotThrown<Exception>(() => tracer.Write(Category.Error, null, "message"));
+        }
+
+        [Fact]
+        public void WriteThrowsIfEntryIsNull()
+        {
+            EmptyTracer tracer = new EmptyTracer();
+            Assert.Throws<ArgumentNullException>(() => tracer.Write(null));
+        }
+
+        [Fact]
+        public void IsCategoryEnabledAlwaysReturnsFalse()
+        {
+            EmptyTracer tracer = new EmptyTracer();
+            bool isEnabled = tracer.IsCategoryEnabled(Category.Debug);
+            Assert.False(isEnabled);
+
+            isEnabled = tracer.IsCategoryEnabled(Category.Information);
+            Assert.False(isEnabled);
+
+            isEnabled = tracer.IsCategoryEnabled(Category.Warning);
+            Assert.False(isEnabled);
+
+            isEnabled = tracer.IsCategoryEnabled(Category.Error);
+            Assert.False(isEnabled);
+        }
+
+        [Fact]
+        public void WriteCoreNotThrowsIfEntryIsNull()
+        {
+            EmptyTracerMock tracer = new EmptyTracerMock();
+            // TODO GATH: Remove? // TODO GATH: Remove?? Assert.IsNotThrown<ArgumentException>(() => tracer.WriteCoreMethod(null));
+        }
+
+        public class EmptyTracerMock : EmptyTracer
+        {
+            public void WriteCoreMethod(TraceEntry entry)
+            {
+                this.WriteCore(entry);
+            }
+        }
+    }
+}
---
+++ Tests/Tracing.Tests/TracerTests.cs
@@ -0,0 +1,195 @@
+﻿using System;
+
+using Moq;
+
+using Xunit;
+
+namespace Tracing.Tests
+{
+    [Collection("Tracing")]
+    public class TracerTests : IDisposable
+    {
+        public TracerTests()
+        {
+            Tracer.Initialize();
+        }
+
+        public void Dispose()
+        {
+            Tracer.Initialize();
+        }
+
+        [Fact]
+        public void ShouldReturnDefaultFactoryIfNoFactoryIsSet()
+        {
+            // Act
+            var factory = Tracer.Factory;
+
+            // Assert
+            Assert.IsType<ConsoleTracerFactory>(factory);
+        }
+
+        [Fact]
+        public void ShouldSetFactory()
+        {
+            // Arrange
+            MockRepository mocks = new MockRepository(MockBehavior.Strict);
+            Mock<ITracerFactory> factoryMock = mocks.Create<ITracerFactory>();
+
+            // Act
+            Tracer.SetFactory(factoryMock.Object);
+
+            // Assert
+            Assert.Same(factoryMock.Object, Tracer.Factory);
+            mocks.VerifyAll();
+        }
+
+        [Fact]
+        public void ShouldReturnDefaultFactoryIfFactoryIsSetNull()
+        {
+            // Arrange
+            MockRepository mocks = new MockRepository(MockBehavior.Strict);
+            Mock<ITracerFactory> factoryMock = mocks.Create<ITracerFactory>();
+
+            Tracer.SetFactory(factoryMock.Object);
+            Assert.Same(factoryMock.Object, Tracer.Factory);
+
+            // Act
+            Tracer.SetFactory(null);
+
+            // Assert
+            Assert.IsType<EmptyTracerFactory>(Tracer.Factory);
+            mocks.VerifyAll();
+        }
+
+        [Fact]
+        public void ShouldOverrideDefaultFactory()
+        {
+            // Arrange
+            MockRepository mocks = new MockRepository(MockBehavior.Strict);
+            Mock<ITracerFactory> factoryMock = mocks.Create<ITracerFactory>();
+            Tracer.SetFactory(null);
+
+            // Act
+            Tracer.SetDefaultFactory(factoryMock.Object);
+
+            // Assert
+            Assert.Same(factoryMock.Object, Tracer.Factory);
+            mocks.VerifyAll();
+        }
+
+        [Fact]
+        public void CreateWithNameCallsCreateOnFactory()
+        {
+            // Arrange
+            MockRepository mocks = new MockRepository(MockBehavior.Strict);
+            Mock<ITracerFactory> factoryMock = mocks.Create<ITracerFactory>();
+            Mock<ITracer> tracerMock = mocks.Create<ITracer>();
+
+            factoryMock.Setup(factory => factory.Create("Name")).Returns(tracerMock.Object);
+
+            Tracer.SetFactory(factoryMock.Object);
+
+            // Act
+            Tracer.Create("Name");
+
+            // Assert
+            mocks.VerifyAll();
+        }
+
+        [Fact]
+        public void CreateWithNameReturnsTracerReturnedByFactory()
+        {
+            MockRepository mocks = new MockRepository(MockBehavior.Strict);
+            Mock<ITracerFactory> factoryMock = mocks.Create<ITracerFactory>();
+            Mock<ITracer> tracerMock = mocks.Create<ITracer>();
+
+            factoryMock.Setup(factory => factory.Create("Name")).Returns(tracerMock.Object);
+
+            Tracer.SetFactory(factoryMock.Object);
+            ITracer tracer = Tracer.Create("Name");
+            Assert.Same(tracerMock.Object, tracer);
+
+            mocks.VerifyAll();
+        }
+
+        [Fact]
+        public void CreateWithTypeCallsCreateOnFactory()
+        {
+            MockRepository mocks = new MockRepository(MockBehavior.Strict);
+            Mock<ITracerFactory> factoryMock = mocks.Create<ITracerFactory>();
+            Mock<ITracer> tracerMock = mocks.Create<ITracer>();
+
+            factoryMock.Setup(factory => factory.Create(typeof(Type))).Returns(tracerMock.Object);
+
+            Tracer.SetFactory(factoryMock.Object);
+            Tracer.Create(typeof(Type));
+
+            mocks.VerifyAll();
+        }
+
+        [Fact]
+        public void CreateWithTypeReturnsTracerReturnedByFactory()
+        {
+            MockRepository mocks = new MockRepository(MockBehavior.Strict);
+            Mock<ITracerFactory> factoryMock = mocks.Create<ITracerFactory>();
+            Mock<ITracer> tracerMock = mocks.Create<ITracer>();
+
+            factoryMock.Setup(factory => factory.Create(typeof(Type))).Returns(tracerMock.Object);
+
+            Tracer.SetFactory(factoryMock.Object);
+            ITracer tracer = Tracer.Create(typeof(Type));
+            Assert.Same(tracerMock.Object, tracer);
+
+            mocks.VerifyAll();
+        }
+
+        [Fact]
+        public void CreateWithGenericTypeCallsCreateOnFactory()
+        {
+            MockRepository mocks = new MockRepository(MockBehavior.Strict);
+            Mock<ITracerFactory> factoryMock = mocks.Create<ITracerFactory>();
+            Mock<ITracer> tracerMock = mocks.Create<ITracer>();
+
+            factoryMock.Setup(factory => factory.Create<Type>()).Returns(tracerMock.Object);
+
+            Tracer.SetFactory(factoryMock.Object);
+            Tracer.Create<Type>();
+
+            mocks.VerifyAll();
+        }
+
+        [Fact]
+        public void CreateWithGenericTypeReturnsTracerReturnedByFactory()
+        {
+            MockRepository mocks = new MockRepository(MockBehavior.Strict);
+            Mock<ITracerFactory> factoryMock = mocks.Create<ITracerFactory>();
+            Mock<ITracer> tracerMock = mocks.Create<ITracer>();
+
+            factoryMock.Setup(factory => factory.Create<Type>()).Returns(tracerMock.Object);
+
+            Tracer.SetFactory(factoryMock.Object);
+            ITracer tracer = Tracer.Create<Type>();
+            Assert.Same(tracerMock.Object, tracer);
+
+            mocks.VerifyAll();
+        }
+
+        [Fact]
+        public void TestCodeForReadme()
+        {
+            // Create an ITracer with 'this' as target name
+            ITracer tracer = Tracer.Create(this);
+
+            // Create an ITracer with MyClass as generic target name
+            ITracer tracerGeneric = Tracer.Create<MyClass>();
+
+            // Create an ITracer with "MyClass" string as target name
+            ITracer tracerStringName = Tracer.Create("MyClass");
+        }
+
+        public class MyClass
+        {
+        }
+    }
+}
